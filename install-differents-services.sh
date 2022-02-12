41566fee7d6088a30e8de880fadc56516b0c3562
Klos54 <theo.faye@hotmail.fr>
2022-02-12 14:58:09 +0100

    Update install-differents-services.sh

--- install-differents-services.sh
+++ install-differents-services.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-read -p "Choose a number on this list:
-1 - Install Nginx, Mariadb and Vsftpd
-2 - Install Zabbix Server v5.4, Mariadb, Nginx with php-fpm v7.4
-3 - Deploy Zabbix Agent to connect to Zabbix Server
-4 - Install GLPI (in a short time)
-5 - OpenVPN
-6 - WireGuard
+if [ "$(whoami)" != "root" ] ; then
+	echo "Please run as root"
+	exit
+fi
+
+read -p "	Choose a number on this list:
+	1 - Install Nginx, Mariadb and Vsftpd
+	2 - Install Zabbix Server v5.4, Mariadb, Nginx with php-fpm v7.4
+	3 - Deploy Zabbix Agent to connect to Zabbix Server
+	4 - Install GLPI (in a short time)
+	5 - OpenVPN
+	6 - WireGuard
 " choise
 
 case $choise in
@@ -19,14 +24,14 @@ case $choise in
 	sed -i 's/index index.html index.htm index.nginx-debian.html;/index index.php index.html index.htm;/' /etc/nginx/sites-enabled/default
 	systemctl restart nginx
 	echo "<?php phpinfo(); ?>" > /var/www/html/index.php
-	#sed -i 's/listen=NO/listen=YES/' /etc/vsftpd.conf
-	#sed -i 's/listen_ipv6=YES/listen_ipv6=NO/' /etc/vsftpd.conf
-	#sed -i 's/#write_enable=YES/write_enable=YES/' /etc/vsftpd.conf
-	#sed -i 's/#local_umask=022/local_umask=022/' /etc/vsftpd.conf
-	#sed -i 's/#chroot_local_user=YES/chroot_local_user=YES/' /etc/vsftpd.conf
-	#sed -i 's/#chroot_list_enable=YES/chroot_list_enable=YES/' /etc/vsftpd.conf
-	#sed -i 's/#chroot_list_file=/etc/vsftpd.chroot_list/chroot_list_file=/etc/vsftpd.chroot_list/' /etc/vsftpd.conf
-	#sed -i 's/ssl_enable=NO/ssl_enable=YES/' /etc/vsftpd.conf
+	sed -i 's/listen=NO/listen=YES/' /etc/vsftpd.conf
+	sed -i 's/listen_ipv6=YES/listen_ipv6=NO/' /etc/vsftpd.conf
+	sed -i 's/#write_enable=YES/write_enable=YES/' /etc/vsftpd.conf
+	sed -i 's/#local_umask=022/local_umask=022/' /etc/vsftpd.conf
+	sed -i 's/#chroot_local_user=YES/chroot_local_user=YES/' /etc/vsftpd.conf
+	sed -i 's/#chroot_list_enable=YES/chroot_list_enable=YES/' /etc/vsftpd.conf
+	sed -i 's/#chroot_list_file=\/etc\/vsftpd.chroot_list/chroot_list_file=\/etc\/vsftpd.chroot_list/' /etc/vsftpd.conf
+	sed -i 's/ssl_enable=NO/ssl_enable=YES/' /etc/vsftpd.conf
 	;;
 	
 	2)
@@ -67,6 +72,23 @@ case $choise in
 	;;
 	
 	4)
+	apt update
+	apt upgrade -y
+	apt install -y nginx php7.4 php7.4-fpm php-mysqli php-mbstring php-curl php-gd php-simplexml php-intl php-ldap php-apcu php-xmlrpc php-zip php-bz2 mariadb-server
+	#In a short time
+	systemctl restart nginx
+	read -p "You must create a new SQL user to manager GLPI:
+	" user
+	read -p "Password for $user ?
+	" password
+	echo "CREATE DATABSE namedb;" | mysql
+	echo "GRANT ALL PRIVILEGES ON glpidb.* TO 'glpiuser'@'localhost' IDENTIFIED BY 'password';" | mysql
+	echo "FLUSH PRIVILEGES;" | mysql
+	wget https://github.com/glpi-project/glpi/releases/download/9.5.7/glpi-9.5.7.tgz
+	tar -xvf glpi-*.tgz
+	mv glpi /var/www/html/
+	chown -R www-data:www-data /var/www/html/
+	chmod -R 755 /var/www/html/
 	;;
 	
 	5)
