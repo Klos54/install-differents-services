56534ef0c1b88646881140fd2e69e89c587b22ff
Klos54 <theo.faye@hotmail.fr>
2022-02-14 20:53:53 +0100

    Update install-differents-services.sh

--- install-differents-services.sh
+++ install-differents-services.sh
@@ -22,11 +22,10 @@ case $choise in
 	apt install -y nginx mariadb-server php-fpm vsftpd
 	sed -i 56,63{'s/#//;s/fastcgi_pass 127.0.0.1:9000\;/#fastcgi_pass 127.0.0.1:9000\;/'} /etc/nginx/sites-enabled/default
 	sed -i 's/index index.html index.htm index.nginx-debian.html;/index index.php index.html index.htm;/' /etc/nginx/sites-enabled/default
-	sed -i {'s/listen=NO/listen=YES/;s/listen_ipv6=YES/listen_ipv6=NO/;s/#write_enable=YES/write_enable=YES/;s/#chroot_local_user=YES/chroot_local_user=YES/;s/#chroot_list_enable=YES/chroot_list_enable=YES/;s/#chroot_list_file=\/etc\/vsftpd.chroot_list/chroot_list_file=\/etc\/vsftpd\/vsftpd.chroot_list/;s/ssl_enable=NO/ssl_enable=YES/;s/#xferlog_file=\/var\/log\/vsftpd.log/xferlog_file=\/var\/log\/vsftpd.log/;s/#chown_uploads=YES/chown_uploads=YES/;s/#chown_username=whoever/chown_username=www-data/'} /etc/vsftpd.conf
+	sed -i {'s/listen=NO/listen=YES/;s/listen_ipv6=YES/listen_ipv6=NO/;s/#write_enable=YES/write_enable=YES/;s/#chroot_local_user=YES/chroot_local_user=YES/;s/#chroot_list_enable=YES/chroot_list_enable=YES/;s/#chroot_list_file=\/etc\/vsftpd.chroot_list/chroot_list_file=\/etc\/vsftpd\/vsftpd.chroot_list/;s/ssl_enable=NO/ssl_enable=YES/;s/#xferlog_file=\/var\/log\/vsftpd.log/xferlog_file=\/var\/log\/vsftpd.log/;s/#chown_uploads=YES/chown_uploads=YES/;s/#local_umask=022/local_umask=022/'} /etc/vsftpd.conf
 	sed -i 's/ExecStart=\/usr\/sbin\/vsftpd \/etc\/vsftpd.conf/ExecStart=\/usr\/sbin\/vsftpd \/etc\/vsftpd\/vsftpd.conf/' /lib/systemd/system/vsftpd.service
 	echo "allow_writeable_chroot=YES
-dirlist_enable=YES
-file_open_mode=0755" >> /etc/vsftpd.conf
+dirlist_enable=YES" >> /etc/vsftpd.conf
 	echo "<?php phpinfo(); ?>" > /var/www/html/index.php
 	read -p "You must create the first account for FTP :
 What's the username ?
@@ -35,10 +34,9 @@ What's the username ?
 	mkdir /etc/vsftpd
 	mv /etc/vsftpd.conf /etc/vsftpd
 	echo $username > /etc/vsftpd/vsftpd.chroot_list
-	usermod -g www-data $username
-	usermod -a -G $username $username
-	chown -R www-data:www-data /var/www/html/
-	chmod -R 755 /var/www/html/
+	usermod -a -G www-data $username
+	chown -R www-data:www-data /var/www
+	chmod -R 755 /var/www
 	systemctl daemon-reload
 	systemctl restart nginx vsftpd
 	;;
