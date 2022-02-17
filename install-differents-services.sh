9d351b0a8d2f1031e0360b0e9418553142778c9e
Klos54 <86574353+Klos54@users.noreply.github.com>
2022-02-17 05:31:36 -0500

    Update install-differents-services.sh

--- install-differents-services.sh
+++ install-differents-services.sh
@@ -24,7 +24,7 @@ case $choise in
 	apt install -y nginx mariadb-server php-fpm vsftpd php-mysql
 	rm -rf /var/www/html
 	sed -i 56,63{'s/#//;s/fastcgi_pass 127.0.0.1:9000\;/#fastcgi_pass 127.0.0.1:9000\;/'} /etc/nginx/sites-enabled/default
-	sed -i 's/index index.html index.htm index.nginx-debian.html\;/index index.php index.html index.htm\;/;s/root /var/www/html\;/root /var/www\;/' /etc/nginx/sites-enabled/default
+	sed -i {'s/index index.html index.htm index.nginx-debian.html;/index index.php index.html index.htm;/;s/\/root \/var\/www\/html;/\/root \/var\/www/'} /etc/nginx/sites-enabled/default
 	sed -i {'s/listen=NO/listen=YES/;s/listen_ipv6=YES/listen_ipv6=NO/;s/#write_enable=YES/write_enable=YES/;s/#chroot_local_user=YES/chroot_local_user=YES/;s/#chroot_list_enable=YES/chroot_list_enable=YES/;s/#chroot_list_file=\/etc\/vsftpd.chroot_list/chroot_list_file=\/etc\/vsftpd\/vsftpd.chroot_list/;s/ssl_enable=NO/ssl_enable=YES/;s/#xferlog_file=\/var\/log\/vsftpd.log/xferlog_file=\/var\/log\/vsftpd.log/;s/#chown_uploads=YES/chown_uploads=YES/;s/#local_umask=022/local_umask=022/'} /etc/vsftpd.conf
 	sed -i 's/ExecStart=\/usr\/sbin\/vsftpd \/etc\/vsftpd.conf/ExecStart=\/usr\/sbin\/vsftpd \/etc\/vsftpd\/vsftpd.conf/' /lib/systemd/system/vsftpd.service
 	echo "allow_writeable_chroot=YES
