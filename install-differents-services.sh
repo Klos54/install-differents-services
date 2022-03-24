ed6d65470d5b9deae24aa9f92264e4b34e9678a5
Klos54 <86574353+Klos54@users.noreply.github.com>
2022-03-24 20:58:01 +0000

    Update install-differents-services.sh

--- install-differents-services.sh
+++ install-differents-services.sh
@@ -66,8 +66,7 @@ What's the username ?
 	echo "grant all privileges on zabbix.* to zabbix@localhost;" | mysql
 	echo "Enter again your password recently created"
 	zcat /usr/share/doc/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uzabbix -p zabbix
-	sed -i {'s/# DBPassword=/# DBPassword=
-DBPassword\=$password/;s/listen 80 default_server\;/#listen 80 default_server\;/s/listen [::]:80 default_server\;/#listen [::]:80 default_server\;/'} /etc/zabbix/zabbix_server.conf
+	sed -i {'s/# DBPassword=/DBPassword\=$password/'} /etc/zabbix/zabbix_server.conf
 	systemctl restart zabbix-server zabbix-agent nginx php7.4-fpm
 	systemctl enable zabbix-server zabbix-agent nginx php7.4-fpm
 	dpkg-reconfigure locales
